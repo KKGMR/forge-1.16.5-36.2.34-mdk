f0d1354b5bb6b82d7e3c2f5a2ad3aa85cf549f56
KKGMR <KKthegod88@gmail.com>
2023-10-19 20:32:47 -0400

    Added tool tip

--- src/main/java/com/KK/iflwwos/item/custom/PowerGlove.java
+++ src/main/java/com/KK/iflwwos/item/custom/PowerGlove.java
@@ -6,9 +6,12 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUseContext;
-import net.minecraft.util.ActionResultType;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.Explosion;
@@ -17,10 +20,8 @@ import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.UUID;
 
 public class PowerGlove extends Item {
-    private static final UUID funny = null;
 
     @Override
     public boolean showDurabilityBar(ItemStack stack) {
@@ -30,6 +31,7 @@ public class PowerGlove extends Item {
     public PowerGlove(Properties properties) {
         super(properties);
     }
+
 
     @Override
     public boolean getIsRepairable(ItemStack par1ItemStack, ItemStack par2ItemStack)
@@ -42,20 +44,7 @@ public class PowerGlove extends Item {
         tooltip.add(new TranslationTextComponent("tooltip.iflwwos.power_glove"));
     }
 
-    @Override
-    public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context) {
-        PlayerEntity player = context.getPlayer();
-        World world = context.getWorld();
 
-        if ((!world.isRemote) && (getDamage(stack) < 4)) {
-            charge(player, context, world);
-        }
-
-        return super.onItemUseFirst(stack, context);
-    }
-
-    private void charge(PlayerEntity player, ItemUseContext context, World world) {
-    }
 
     @Override
     public boolean onLeftClickEntity(ItemStack stack, PlayerEntity player, Entity entity) {
@@ -71,16 +60,36 @@ public class PowerGlove extends Item {
         return super.onLeftClickEntity(stack, player, entity);
     }
 
-    private Object nothing() {
-        return null;
+
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
+
+        ItemStack stack = player.getHeldItemMainhand();
+        if ((!world.isRemote) && (getDamage(stack) < 4)) {
+            charge(player);
+            player.addPotionEffect(new EffectInstance(Effects.SLOW_FALLING, 200, 1, false, false));
+            setDamage(stack, getDamage(stack) + 1);
+        }
+        return super.onItemRightClick(world, player, hand);
     }
-
-
 
 
     private void explode(World world, PlayerEntity player, DamageSource damageSource, ExplosionContext explosionContext, Entity entity) {
         Explosion.Mode blockInteraction = null;
         world.createExplosion(player, damageSource, explosionContext, entity.getPosX(),entity.getPosY(), entity.getPosZ(), 8.0F, false, blockInteraction);
+    }
+
+    private void charge(PlayerEntity player) {
+        float yaw = player.rotationYaw;
+        float pitch = player.rotationPitch;
+        float f = 5.0F;
+        double motionX = (-MathHelper.sin(yaw / 180.0F * (float)Math.PI) * MathHelper.cos(pitch / 180.0F * (float)Math.PI) * f);
+        double motionZ = (MathHelper.cos(yaw / 180.0F * (float)Math.PI) * MathHelper.cos(pitch / 180.0F * (float)Math.PI) * f);
+        double motionY = (-MathHelper.sin((pitch) / 180.0F * (float)Math.PI) * f);
+        player.setVelocity(motionX, motionY, motionZ);
+
+
     }
 
 
